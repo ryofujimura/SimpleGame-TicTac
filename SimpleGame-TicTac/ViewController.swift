38dfeef653fce81fcc4119f1f7f963991743efb5
Ryo Fuj <ryo176999@gmail.com>
2022-01-27 23:55:40 -0800

    tic tac toe design and flip button
    design - complete
    button - flips, but need to change image ocnfiguration

--- SimpleGame-TicTac/ViewController.swift
+++ SimpleGame-TicTac/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var activePlayer = 1
+    let config = UIImage.SymbolConfiguration(font: System 80.0 )
+    
+    @IBAction func actionTic(_ sender: UIButton) {
+        if (activePlayer == 1){
+            sender.setImage(UIImage(systemName: "cross.fill", withConfiguration: config), for: UIControl.State())
+            activePlayer = 2
+        }
+        else{
+            sender.setImage(UIImage(systemName: "cross.circle.fill"), for: UIControl.State())
+            activePlayer = 1
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
