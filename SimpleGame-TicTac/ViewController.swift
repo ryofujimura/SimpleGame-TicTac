ab5135be45c0fcc73b71ecab1743a98925697538
Ryo Fuj <ryo176999@gmail.com>
2022-01-27 21:13:13 -0800

    init XCode project

---
+++ SimpleGame-TicTac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleGame-TicTac
+//
+//  Created by ryo fuj on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
